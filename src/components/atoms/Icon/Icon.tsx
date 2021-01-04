1a05af7d72780c4a443e55e9f8a6287b8243e8ff
Andres Aguirre <aguirregzz97@gmail.com>
2021-01-04 00:53:08 -0600

    :sparkles: Add Icon Atom

---
+++ src/components/atoms/Icon/Icon.tsx
@@ -0,0 +1,43 @@
+import './Icon.scss'
+
+import * as React from 'react'
+
+export type IconType = 'Icon_menu' | 'Icon_close'
+
+export type IconSize = 'sm' | 'md' | 'lg'
+export type IconColor = 'grey' | 'yellow' | 'white' | 'black'
+
+export type IconProps = {
+  type: IconType
+  size?: IconSize
+  color?: IconColor
+  className?: string
+}
+
+const ICON_SIZES: Readonly<Record<IconSize, string>> = {
+  sm: 'Icon_sm',
+  md: 'Icon_md',
+  lg: 'Icon_lg',
+}
+
+const ICON_COLORS: Readonly<Record<IconColor, string>> = {
+  grey: 'Icon_grey',
+  yellow: 'Icon_yellow',
+  white: 'Icon_white',
+  black: 'Icon_black',
+}
+
+const Icon: React.FC<IconProps> = ({
+  type,
+  size = 'md',
+  color = 'grey',
+  className,
+}) => {
+  return (
+    <div
+      className={`Icon ${className} ${type} ${ICON_SIZES[size]} ${ICON_COLORS[color]}`}
+    />
+  )
+}
+
+export default Icon
