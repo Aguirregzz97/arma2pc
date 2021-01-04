003de679f984cd5176296093327de1059c55c55f
Andres Aguirre <aguirregzz97@gmail.com>
2021-01-04 00:54:12 -0600

    :sparkles: Add FooterMobile molecule

---
+++ src/components/molecules/FooterMobile/FooterMobile.tsx
@@ -0,0 +1,9 @@
+import './FooterMobile.scss'
+
+import * as React from 'react'
+
+const FooterMobile: React.FC = () => {
+  return <p className='Mobile_footer'>Arma2PC.com</p>
+}
+
+export default FooterMobile
