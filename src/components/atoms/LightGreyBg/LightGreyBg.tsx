770d0d85600df6319256f2604f539a1ef7bcd9a1
Andres Aguirre <aguirregzz97@gmail.com>
2021-01-04 00:53:49 -0600

    :sparkles: Add light grey background atom

---
+++ src/components/atoms/LightGreyBg/LightGreyBg.tsx
@@ -0,0 +1,9 @@
+import './LightGreyBg.scss'
+
+import * as React from 'react'
+
+const LightGreyBg: React.FC = ({ children }) => {
+  return <div className='LightGrey_bg'>{children}</div>
+}
+
+export default LightGreyBg
