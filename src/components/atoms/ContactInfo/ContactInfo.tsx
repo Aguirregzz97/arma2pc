6a767924696764ef2279dbb9085896a24fbe2ce3
Andres Aguirre <aguirregzz97@gmail.com>
2021-01-06 04:46:50 -0600

    :sparkles: Add ContactInfo atom

---
+++ src/components/atoms/ContactInfo/ContactInfo.tsx
@@ -0,0 +1,34 @@
+import './ContactInfo.scss'
+
+import * as React from 'react'
+
+type ContactMediaProps = {
+  name: string
+  value: string
+}
+
+const ContactMedia: React.FC<ContactMediaProps> = ({ name, value }) => {
+  return (
+    <div className='ContactMedia_container'>
+      <p>{name}</p>
+      <p>{value}</p>
+    </div>
+  )
+}
+
+const ContactInfo: React.FC = () => {
+  return (
+    <div className='ContactInfo_container'>
+      <div style={{ display: 'inline-block' }}>
+        <h1>Contacto</h1>
+      </div>
+      <p>
+        El entusiasmo y dedicacion de los trabajadores de arma2PC.com son la
+        clave para la mejor posible PC que puedes armar en el momento
+      </p>
+      <ContactMedia name='E-mail' value='aguirregzz97@gmail.com' />
+    </div>
+  )
+}
+
+export default ContactInfo
