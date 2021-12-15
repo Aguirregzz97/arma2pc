7b65490352038ef6066777731d8a7bc65ab1a680
Andres Aguirre <andres.aguirre@common.com>
2021-12-15 13:12:42 -0600

    ♻️ Change email to stop paying service

--- src/components/atoms/ContactInfo/ContactInfo.tsx
+++ src/components/atoms/ContactInfo/ContactInfo.tsx
@@ -3,35 +3,35 @@ import "./ContactInfo.scss";
 import * as React from "react";
 
 type ContactMediaProps = {
-	name: string;
-	value: string;
+  name: string;
+  value: string;
 };
 
 const ContactMedia: React.FC<ContactMediaProps> = ({ name, value }) => {
-	return (
-		<div className="ContactMedia_container">
-			<p>{name}</p>
-			<p>{value}</p>
-		</div>
-	);
+  return (
+    <div className="ContactMedia_container">
+      <p>{name}</p>
+      <p>{value}</p>
+    </div>
+  );
 };
 
 const ContactInfo: React.FC = () => {
-	return (
-		<div className="ContactInfo_container">
-			<div style={{ display: "inline-block" }}>
-				<h1>Contacto</h1>
-			</div>
-			<p>
-				El entusiasmo y dedicacion de los trabajadores de arma2PC.com son la
-				clave para la mejor posible PC que puedes armar en el momento
-			</p>
-			<ContactMedia
-				name="E-mail - telefono"
-				value="contacto@arma2pc.com 8110223215"
-			/>
-		</div>
-	);
+  return (
+    <div className="ContactInfo_container">
+      <div style={{ display: "inline-block" }}>
+        <h1>Contacto</h1>
+      </div>
+      <p>
+        El entusiasmo y dedicacion de los trabajadores de arma2PC.com son la
+        clave para la mejor posible PC que puedes armar en el momento
+      </p>
+      <ContactMedia
+        name="E-mail - telefono"
+        value="aguirregzz97@gmail.com 8110223215"
+      />
+    </div>
+  );
 };
 
 export default ContactInfo;
